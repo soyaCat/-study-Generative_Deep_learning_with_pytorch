35f1e7abdfeed0c33790b2a9d94bc2995b8051ba
soya <soyamilk432@gmail.com>
2021-02-01 02:54:38 +0900

    ADD Generate image from dims points function

--- face_VAE.py
+++ face_VAE.py
@@ -16,22 +16,26 @@ from tqdm import tqdm
 
 batch_size = 64
 lr = 0.0005
-trainEpochs = 20
-showPointEpochs = 0
-testEpochs = 1
+trainEpochs = 0
+generateEpochs = 30
+testEpochs = 0
 r_loss_factor = 10000
 z_dims = 200
 
 train_mode = True
-load_model = False
-save_model = True
+load_model = True
+save_model = False
 date_time = datetime.datetime.now().strftime("%Y%m%d-%H-%M-%S")
 save_path = "./saved_face_VAE_model/"+date_time+"/model/"
-load_path = "./saved_face_VAE_model/"+"20210124-23-33-26"+"/model/"
+load_path = "./saved_face_VAE_model/"+"20210131-18-18-11"+"/model/"
 
 celebA_train = dset.CelebA("./", split='train', transform=transforms.ToTensor(),
                         target_transform=None, download=False)
 train_loader = torch.utils.data.DataLoader(celebA_train, batch_size=batch_size, 
+                                            shuffle=True, num_workers=2, drop_last=True)
+celebA_test = dset.CelebA("./", split='test', transform=transforms.ToTensor(),
+                        target_transform=None, download=False)
+test_loader = torch.utils.data.DataLoader(celebA_test, batch_size=200, 
                                             shuffle=True, num_workers=2, drop_last=True)
 
 class image_process():
@@ -100,9 +104,8 @@ class AutoEncoder_model(nn.Module):
 
     def decode(self, x):
         decoder_input = self.d_fc1(x)
-        decoder_input = torch.reshape(decoder_input, shape=[-1,64, 4, 4]).to(self.device)
+        decoder_input = torch.reshape(decoder_input, shape=[-1,64, 28, 23]).to(self.device)
         decoder_out = self.Decoder(decoder_input)
-        decoder_out = torch.reshape(decoder_out, shape=[-1, 1, 28, 28]).to(self.device)
         return decoder_out
 
 
@@ -162,11 +165,118 @@ class VAE():
             decoder_out, encoder_out , _, _ = self.model.forward(x)
             return encoder_out
 
+    def get_result_from_point(self, vector):
+        self.model.eval()
+        with torch.no_grad():
+            x = vector.to(self.device)
+            decoder_out = self.model.decode(x)
+            return decoder_out[0]
+
+class Generate_from_point():
+    def __init__(self, VAE):
+        self.VAE = VAE
+
+    def get_label_and_vector_list(self, test_loader):
+        label_list = []
+        Vector_list = []
+        print("\n", "get label_list and vector_list from model...")
+        for index,[image, label] in enumerate(tqdm(test_loader)):
+            x = image.to(self.VAE.device)
+            label = label.data.numpy()
+            for i in range(np.shape(label)[0]):
+                label_list.append(label[i,:])
+            encoder_out = self.VAE.get_result_with_point(image)
+            encoder_out = encoder_out.cpu().data.numpy()
+            for i in range(np.shape(encoder_out)[0]):
+                Vector_list.append(encoder_out[i,:])
+
+        return label_list, Vector_list
+
+    def get_feature_vector(self, feature_num, label_list, Vector_list):
+        '''
+        0: 5_o_Clock_Shadow 
+        1: Arched_Eyebrows 
+        2: Attractive 
+        3: Bags_Under_Eyes 
+        4: Bald 
+        5: Bangs 
+        6: Big_Lips 
+        7: Big_Nose 
+        8: Black_Hair 
+        9: Blond_Hair 
+        10: Blurry 
+        11: Brown_Hair 
+        12: Bushy_Eyebrows 
+        13: Chubby 
+        14: Double_Chin 
+        15: Eyeglasses 
+        16: Goatee 
+        17: Gray_Hair 
+        18: Heavy_Makeup 
+        19: High_Cheekbones 
+        20: Male 
+        21: Mouth_Slightly_Open 
+        22: Mustache 
+        23: Narrow_Eyes 
+        24: No_Beard 
+        25: Oval_Face 
+        26: Pale_Skin 
+        27: Pointy_Nose 
+        28: Receding_Hairline 
+        29: Rosy_Cheeks 
+        30: Sideburns 
+        31: Smiling 
+        32: Straight_Hair 
+        33: Wavy_Hair 
+        34: Wearing_Earrings 
+        35: Wearing_Hat 
+        36: Wearing_Lipstick 
+        37: Wearing_Necklace 
+        38: Wearing_Necktie
+        39: Young
+        '''
+        feature_list = []
+        none_feature_list = []
+        for index,label in enumerate(label_list):
+            if(label[feature_num] == 1):
+                feature_list.append(Vector_list[index])
+            else:
+                none_feature_list.append(Vector_list[index])
+        
+        feature_mean = np.mean(feature_list, axis = 0)
+        none_feature_mean = np.mean(none_feature_list, axis = 0)
+        result_vector = feature_mean - none_feature_mean
+
+        return result_vector
+
+    def get_random_img(self, test_loader, except_feature_num):
+        finish_indexing = False
+        target_img = 0
+        for index,[image, label] in enumerate(test_loader):
+            x = image.to(self.VAE.device)
+            label = label.data.numpy()
+            for i in range(np.shape(label)[0]):
+                if(label[i][except_feature_num] == 0):
+                    target_img = x[i]
+                    encoder_out = self.VAE.get_result_with_point(x)
+                    target_Vector = encoder_out.cpu().data.numpy()[i]
+                    finish_indexing = True
+                    break
+            if (finish_indexing == True):
+                break
+
+        return target_img, target_Vector
+                    
+
+
+
+
 
 if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     VAE = VAE(device)
     image_process = image_process()
+    Generate_from_point = Generate_from_point(VAE)
 
     for epoch in range(trainEpochs):
         total_loss, r_loss, kl_loss = VAE.train_model(train_loader)
@@ -182,30 +292,24 @@ if __name__ == '__main__':
         torch.save(VAE.model.state_dict(), save_path+"state_dict_model.pt")
         print("Model saved..")
     
-    for epoch in range(showPointEpochs):
-        axis_x_list = []
-        axis_y_list = []
-        number_list = []
-        color_table = ['b','g','r','c','m','y','k','tan','brown','blueviolet']
-        i = 0
-        for index,[image, label] in enumerate(train_loader):
-            i+=1
-            encoder_out = VAE.get_result_with_point(image)
-            #print(encoder_out.size())#torch.Size([1024, 2])
-            #print(label.size())#torch.Size([1024])
-            print("stack points...")
-            encoder_out = encoder_out.cpu().data.numpy()
-            for index, number in enumerate(label):
-                axis_x_list.append(encoder_out[index][0])
-                axis_y_list.append(encoder_out[index][1])
-                number_list.append(color_table[number])
-            if i % 5 == 0:
-                i = 0
-                plt.scatter(axis_x_list, axis_y_list, c = number_list, s = 2)
-                plt.show()
-                axis_x_list = []
-                axis_y_list = []
-                number_list = []
+    for epoch in range(generateEpochs):
+        if(epoch == 0):
+            label_list, Vector_list = Generate_from_point.get_label_and_vector_list(test_loader)
+        feature_vector = Generate_from_point.get_feature_vector(39, label_list, Vector_list)
+        print("selecting image from data sets....")
+        img, target_Vector = Generate_from_point.get_random_img(test_loader, 39)
+        for level in range(8):
+            print("Vector adjustment level: ", level)
+            target_Vector = target_Vector + level*feature_vector*0.2
+            target_Vector_torch = torch.from_numpy(target_Vector)
+            img = VAE.get_result_from_point(target_Vector_torch)
+            img = image_process.image_postprocess(img.cpu()).data.numpy()
+            plt.figure(figsize=(5,5))
+            plt.imshow(img)
+            plt.show()
+
+
+
 
     for epoch in range(testEpochs):
         for image, label in train_loader:
